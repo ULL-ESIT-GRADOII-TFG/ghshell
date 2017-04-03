bd9b52119eb2a9994ed783730eb3c4d7490fce64
Juan José <jjlabradorglez@gmail.com>
2017-04-04 00:14:25 +0100

    First steps with GitHub API

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 #! /usr/bin/env node
 
 const readline = require('readline');
+var colors = require('colors');
+var GitHubApi = require('github');
+
+var github = new GitHubApi({
+    debug: false,
+    protocol: "https",
+    host: "api.github.com",
+    /*headers: {
+        "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+    },*/
+    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+    timeout: 5000
+});
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     completer,
-    prompt: 'ghshell > '
+    prompt: 'ghshell > '.cyan
 });
 
 
@@ -17,16 +31,30 @@ function intro() {
 }
 
 function completer(line) {
-    var completions = 'help list'.split(' ');
+    var completions = 'help list exit repos orgs'.split(' ');
     var hits = completions.filter(function(c) {
-        if (c.indexOf(line) == 0) {
+        if (c.indexOf(line) === 0) {
             return c;
         }
     });
     return [hits && hits.length ? hits : completions, line];
 }
 
+function listOrgs(obj) {
+    console.log('');
+    for(var i = 0; i < obj.data.length; i++)
+        rl.output.write(obj.data[i].login + '   ');
+    rl.write(null, {name: 'enter'});
+}
+
 intro();
+
+github.authenticate({
+    type: "basic",
+    username: "user",
+    password: "password"
+});
+
 rl.prompt();
 
 rl.on('line', function (line) {
@@ -37,9 +65,16 @@ rl.on('line', function (line) {
         case 'list':
             console.log('Show list');
             break;
+        case 'orgs':
+            github.users.getOrgs({}, function (error, result) {
+                listOrgs(result);
+            });
+            break;
+        case 'exit':
+            process.exit(0);
         default:
             //console.log(`'${line.trim()}'`);
-            console.log('Show help');
+            //console.log('Show help');
             break;
     }
     rl.prompt();
@@ -47,17 +82,3 @@ rl.on('line', function (line) {
     console.log('');
     process.exit(0);
 });
-
-
-/*
-var shell = require('commander');
-
-shell
-    .version('1.0.0')
-    .option('-u, --username [user]', 'The user to authenticate as')
-    .option('-p, --password [pass]', "User'\ password")
-    .parse(process.argv);
-
-console.log('User: %j', shell.username);
-console.log('Password: %j', shell.password);
-*/
