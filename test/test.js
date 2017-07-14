ea8e1d052dbe72da5c7673ad2fd60626f220723a
Juan José Labrador González <jjlabradorglez@gmail.com>
2017-07-14 18:38:03 +0100

    Adding some tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,37 @@
-var assert = require('chai').assert;
-var str = require('../index');
+let chai = require('chai');
+let assert = chai.assert;
 
-describe('#str', function () {
-    it ('Should be pending');
+let commands = require('../lib/commands').commands;
+
+describe('Commands', () => {
+    it("should be a valid object", () => {
+        assert.isOk(commands);
+        assert.typeOf(commands, 'object');
+    });
+
+    it("should have a 'main' property", () => {
+        assert.property(commands, 'main');
+    });
+
+    it("should have an 'orgs' property", () => {
+        assert.property(commands, 'orgs');
+    });
+
+    it("should have a 'repos' property", () => {
+        assert.property(commands, 'repos');
+    });
+
+    describe("Commands' properties", () => {
+        it("'main' property should be an array", () => {
+            assert.isArray(commands['main']);
+        });
+
+        it("'orgs' property should be an object", () => {
+            assert.isObject(commands['orgs']);
+        });
+
+        it("'repos' property should be an object", () => {
+            assert.isObject(commands['repos']);
+        });
+    });
 });
